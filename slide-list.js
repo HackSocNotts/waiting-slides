4e92138cc349052f7b9a84f1417763bbc14cc0a8
James C <3661681+JamesMCo@users.noreply.github.com>
2018-10-11 02:09:29 +0100

    Added "signup" hacktoberfest slide, repeated slides in queue

--- slide-list.js
+++ slide-list.js
@@ -2,6 +2,9 @@ slides = [
     "slides/general/hacksoc.html",
     "slides/hacktoberfest-2018/hacktoberfest.html",
     "slides/hacktoberfest-2018/schedule.html",
+    "slides/hacktoberfest-2018/signup.html",
+    "slides/hacktoberfest-2018/hacktoberfest.html",
+    "slides/general/hacksoc.html",
     "slides/general/social.html",
     "slides/general/upcomingevents.html",
     "slides/general/upcomingevents2.html"
