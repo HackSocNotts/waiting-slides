d7e5923d0ac67365363af40aa1720b6667cd10d0
James C <3661681+JamesMCo@users.noreply.github.com>
2018-10-11 17:40:17 +0100

    Added "here to help" slide

--- slide-list.js
+++ slide-list.js
@@ -4,6 +4,8 @@ slides = [
     "slides/hacktoberfest-2018/schedule.html",
     "slides/hacktoberfest-2018/signup.html",
     "slides/hacktoberfest-2018/hacktoberfest.html",
+    "slides/hacktoberfest-2018/heretohelp.html",
+    "slides/hacktoberfest-2018/schedule.html",
     "slides/general/hacksoc.html",
     "slides/general/social.html",
     "slides/general/upcomingevents.html",
