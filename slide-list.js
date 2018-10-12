e0264b729043796bcab1c67582467984fdc36ac9
James C <3661681+JamesMCo@users.noreply.github.com>
2018-10-12 02:35:26 +0100

    Update slide list in preparation for Python Workshop session

--- slide-list.js
+++ slide-list.js
@@ -1,15 +1,7 @@
 slides = [
     "slides/general/hacksoc.html",
-    "slides/hacktoberfest-2018/hacktoberfest.html",
-    "slides/hacktoberfest-2018/schedule.html",
-    "slides/hacktoberfest-2018/welcometalk.html",
-    "slides/hacktoberfest-2018/signup.html",
-    "slides/hacktoberfest-2018/hacktoberfest.html",
-    "slides/hacktoberfest-2018/heretohelp.html",
-    "slides/hacktoberfest-2018/schedule.html",
-    "slides/hacktoberfest-2018/welcometalk.html",
-    "slides/hacktoberfest-2018/community-hacktoberfest-website.html",
-    "slides/general/hacksoc.html",
+    "slides/python-workshops/python.html",
+    "slides/python-workshops/python-worksheet.html",
     "slides/general/social.html",
     "slides/general/upcomingevents.html",
     "slides/general/upcomingevents2.html"
