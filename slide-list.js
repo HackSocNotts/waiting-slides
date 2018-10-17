c139febc3a761cfdc59507a7a82bd643e1db3221
James C <3661681+JamesMCo@users.noreply.github.com>
2018-10-17 01:06:30 +0100

    Added membership slide

--- slide-list.js
+++ slide-list.js
@@ -1,8 +1,10 @@
 slides = [
     "slides/general/hacksoc.html",
+    "slides/general/membershiprequired.html",
     "slides/linux-workshops/linux.html",
     "slides/linux-workshops/linux-worksheet.html",
     "slides/general/social.html",
+    "slides/general/membershiprequired.html",
     "slides/general/upcomingevents.html",
     "slides/general/upcomingevents3.html",
     "slides/general/upcomingevents2.html"
