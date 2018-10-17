cd8fdb54ccbf0220f4cad7854358d6bfd5513acb
James C <3661681+JamesMCo@users.noreply.github.com>
2018-10-17 01:06:09 +0100

    Swapped slides for Linux workshop

--- slide-list.js
+++ slide-list.js
@@ -1,7 +1,7 @@
 slides = [
     "slides/general/hacksoc.html",
-    "slides/python-workshops/python.html",
-    "slides/python-workshops/python-worksheet.html",
+    "slides/linux-workshops/linux.html",
+    "slides/linux-workshops/linux-worksheet.html",
     "slides/general/social.html",
     "slides/general/upcomingevents.html",
     "slides/general/upcomingevents3.html",
