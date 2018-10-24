c19a35500304ffa9cc64b47a5c5bd512faed259a
James C <3661681+JamesMCo@users.noreply.github.com>
2018-10-25 00:15:52 +0100

    Set up slides for 25th Oct Talk to Alexa Workshop

--- slide-list.js
+++ slide-list.js
@@ -1,12 +1,12 @@
 slides = [
     "slides/general/hacksoc.html",
     "slides/general/membershiprequired.html",
-    "slides/python-workshops/python.html",
-    "slides/python-workshops/python-worksheet.html",
+    "slides/general/membershipstickers.html",
+    "slides/speaker-event/stevenpears-talktoalexa.html",
     "slides/general/social.html",
     "slides/general/membershiprequired.html",
+    "slides/general/membershipstickers.html",
     "slides/general/upcomingevents.html",
     "slides/general/upcomingevents3.html",
-    "slides/general/upcomingevents2.html",
-    "slides/hacksheffield/hacksheffield.html"
+    "slides/general/upcomingevents2.html"
 ];
