7d040a86f2f95f2cab13a473ab61425c56a52924
James C <3661681+JamesMCo@users.noreply.github.com>
2018-11-01 13:15:37 +0000

    Updated slides for "Being Part of the Community" talk

--- slide-list.js
+++ slide-list.js
@@ -1,12 +1,11 @@
 slides = [
     "slides/general/hacksoc.html",
-    "slides/general/membershiprequired.html",
-    "slides/general/membershipstickers.html",
-    "slides/python-workshops/python.html",
-    "slides/python-workshops/python-worksheet.html",
+    // "slides/general/membershiprequired.html",
+    // "slides/general/membershipstickers.html",
+    "slides/speaker-event/jessicawhite-beingpartofthecommunity.html",
     "slides/general/social.html",
-    "slides/general/membershiprequired.html",
-    "slides/general/membershipstickers.html",
+    // "slides/general/membershiprequired.html",
+    // "slides/general/membershipstickers.html",
     "slides/general/upcomingevents.html",
     "slides/general/upcomingevents3.html",
     "slides/general/upcomingevents2.html"
