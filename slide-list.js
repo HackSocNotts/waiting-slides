7fb6df8cd965a5796b80060a1ae4501389788ead
James C <3661681+JamesMCo@users.noreply.github.com>
2018-11-06 18:17:36 +0000

    Set up slides for javascript workshop 1

--- slide-list.js
+++ slide-list.js
@@ -1,11 +1,12 @@
 slides = [
     "slides/general/hacksoc.html",
-    // "slides/general/membershiprequired.html",
-    // "slides/general/membershipstickers.html",
-    "slides/speaker-event/jessicawhite-beingpartofthecommunity.html",
+    "slides/general/membershiprequired.html",
+    "slides/general/membershipstickers.html",
+    "slides/javascript-workshops/javascript.html",
+    "slides/javascript-workshops/javascript-worksheet.html",
     "slides/general/social.html",
-    // "slides/general/membershiprequired.html",
-    // "slides/general/membershipstickers.html",
+    "slides/general/membershiprequired.html",
+    "slides/general/membershipstickers.html",
     "slides/general/upcomingevents.html",
     "slides/general/upcomingevents3.html",
     "slides/general/upcomingevents2.html"
