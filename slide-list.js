c2b0c6d4c0bc016e3eae33f39661edea86b4771c
James C <3661681+JamesMCo@users.noreply.github.com>
2018-12-03 23:56:54 +0000

    Updated slides for javascript workshop 4

--- slide-list.js
+++ slide-list.js
@@ -4,10 +4,11 @@ slides = [
     "slides/general/membershipstickers.html",
     "slides/javascript-workshops/javascript.html",
     "slides/javascript-workshops/javascript-worksheet.html",
+    "slides/adventofcode/adventofcode.html",
     "slides/general/social.html",
-    "slides/general/membershiprequired.html",
-    "slides/general/membershipstickers.html",
-    "slides/general/upcomingevents.html",
+    // "slides/general/membershiprequired.html",
+    // "slides/general/membershipstickers.html",
+    // "slides/general/upcomingevents.html",
     "slides/general/upcomingevents3.html",
     // "slides/general/upcomingevents2.html"
 ];
