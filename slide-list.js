fa98045a7cb74024159ff76b2fdf681452e70910
James C <3661681+JamesMCo@users.noreply.github.com>
2019-02-26 17:50:22 +0000

    Updated slides for C# with Unity Localhost event

--- slide-list.js
+++ slide-list.js
@@ -2,12 +2,10 @@ slides = [
     "slides/general/hacksoc.html",
     // "slides/general/membershiprequired.html",
     // "slides/general/membershipstickers.html",
-    "slides/kotlin-workshops/kotlin.html",
-    "slides/kotlin-workshops/kotlin-worksheet.html",
+    "slides/localhost/csharp-with-unity.html",
+    "slides/localhost/checkin.html",
     "slides/general/social.html",
     // "slides/general/membershiprequired.html",
     // "slides/general/membershipstickers.html",
-    // "slides/general/upcomingevents.html",
-    "slides/general/upcomingevents3.html",
-    // "slides/general/upcomingevents2.html"
+    "slides/general/upcomingevents3.html"
 ];
