c1de4b5159e423fd4085947ec255ce4da9536f1f
James C <3661681+JamesMCo@users.noreply.github.com>
2019-03-12 17:49:17 +0000

    Updated slides for cybersecurity workshop

--- slide-list.js
+++ slide-list.js
@@ -2,7 +2,7 @@ slides = [
     "slides/general/hacksoc.html",
     // "slides/general/membershiprequired.html",
     // "slides/general/membershipstickers.html",
-    "slides/speaker-event/leahjervis-myyearinindustry.html",
+    "slides/cybersecurity-workshops/cybersecurity.html",
     "slides/general/social.html",
     // "slides/general/membershiprequired.html",
     // "slides/general/membershipstickers.html",
