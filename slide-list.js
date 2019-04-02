06ed81c647f01d95fc2491e2581acba61ca4c61c
James C <3661681+JamesMCo@users.noreply.github.com>
2019-04-02 17:34:16 +0100

    Updated slides for DL talk

--- slide-list.js
+++ slide-list.js
@@ -2,10 +2,9 @@ slides = [
     "slides/general/hacksoc.html",
     // "slides/general/membershiprequired.html",
     // "slides/general/membershipstickers.html",
-    "slides/cpp-workshops/cpp.html",
+    "slides/speaker-event/alex-olli-deeplearning.html",
     "slides/general/social.html",
     // "slides/general/membershiprequired.html",
     // "slides/general/membershipstickers.html",
-    "slides/general/upcomingevents.html",
-    "slides/general/upcomingevents3.html"
+    "slides/general/upcomingevents.html"
 ];
