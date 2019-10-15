1d17e983172cec84d0ba04119fb9990d73be08a2
James C <3661681+JamesMCo@users.noreply.github.com>
2019-10-15 17:18:35 +0100

    Update slides for Intro to Linux workshop

--- slide-list.js
+++ slide-list.js
@@ -1,11 +1,7 @@
 slides = [
     "slides/general/hacksoc.html",
-    // "slides/general/membershiprequired.html",
-    // "slides/general/membershipstickers.html",
-    "slides/speaker-event/petegallagher-iot.html",
+    "slides/linux-workshops/linux.html",
     "slides/general/social.html",
-    // "slides/general/membershiprequired.html",
-    // "slides/general/membershipstickers.html",
     "upcomingevents",
     "slides/general/upcomingevents_hacknotts.html"
 ];
--- upcoming-events.js
+++ upcoming-events.js
@@ -1,22 +1,32 @@
 upcoming_events = [
 	{
-		"line1": "Saturday 12th October 1:00pm, A32:",
-		"line2": "MLH Local Hack Day (Learn)",
-		"line3": ""
+		"line1": "Thursday October 17th 6:30pm, A32:",
+		"line2": "Speaker Event",
+		"line3": "To Be Confirmed"
 	},
 	{
-		"line1": "Monday October 14th 6:00pm, Antenna:",
-		"line2": "TechNottingham",
-		"line3": "Depart from Exchange @ 17:45"
-	},
-	{
-		"line1": "Tuesday October 15th 6:30pm:",
-		"line2": "Intro to Linux",
-		"line3": ""
-	},
-	{
-		"line1": "Tuesday October 22nd 6:30pm:",
+		"line1": "Tuesday October 22nd 6:30pm, LT3:",
 		"line2": "Cyber Security - Cross Site Scripting",
+		"line3": ""
+	},
+	{
+		"line1": "Thursday October 24th 6:30pm:",
+		"line2": "Hacktoberfest Open Hack Day",
+		"line3": ""
+	},
+	{
+		"line1": "Saturday October 26th 10am, A32:",
+		"line2": "Python Crash Course",
+		"line3": ""
+	},
+	{
+		"line1": "Tuesday October 29th 6:30pm, A32:",
+		"line2": "MLH Localhost",
+		"line3": "Learn C%23 With Unity"
+	},
+	{
+		"line1": "Thursday, October 31st 6:30pm, A32:",
+		"line2": "Hardware Meetup",
 		"line3": ""
 	}
 ]
