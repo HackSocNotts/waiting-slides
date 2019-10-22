a9f8ce5db4a4c0353156b8773272dca1942aea15
Aaron Osher <aaron@osher.dev>
2019-10-22 17:51:44 +0100

    Update form 22/10/19

--- slide-list.js
+++ slide-list.js
@@ -1,7 +1,9 @@
 slides = [
     "slides/general/hacksoc.html",
-    "slides/speaker-event/salmanchishti-microsoftecosystem.html",
+    "slides/cybersecurity-workshops/cybersecurity.html",
     "slides/general/social.html",
     "upcomingevents",
+    "slides/general/hoodies.html",
+    "slides/general/elections.html",
     "slides/general/upcomingevents_hacknotts.html"
 ];
--- upcoming-events.js
+++ upcoming-events.js
@@ -1,32 +1,32 @@
 upcoming_events = [
 	{
-		"line1": "Tuesday October 22nd 6:30pm, LT3:",
-		"line2": "Cyber Security - Cross Site Scripting",
-		"line3": ""
-	},
-	{
-		"line1": "Thursday October 24th 6:30pm:",
+		"line1": "Thursday October 24th 18:30 COMP-A32:",
 		"line2": "Hacktoberfest Open Hack Day",
 		"line3": ""
 	},
 	{
-		"line1": "Thursday October 25th: Time TBC",
-		"line2": "CyberTeam Meet-Up",
-		"line3": ""
-	},
-	{
-		"line1": "Saturday October 26th 10am, A32:",
+		"line1": "Saturday October 26th 10:00, COMP-A32:",
 		"line2": "Python Crash Course",
 		"line3": ""
 	},
 	{
-		"line1": "Tuesday October 29th 6:30pm, A32:",
+		"line1": "Monday October 28th 18:30, COMP-C60:",
+		"line2": "Extraordinary General Meeting",
+		"line3": "hacksoc.net/nominate"
+	},
+	{
+		"line1": "Tuesday October 29th 6:30pm, COMP-A32:",
 		"line2": "MLH Localhost",
 		"line3": "Learn C%23 With Unity"
 	},
 	{
-		"line1": "Thursday, October 31st 6:30pm, A32:",
+		"line1": "Thursday, October 31st 6:30pm, COMP-A32:",
 		"line2": "Hardware Meetup",
 		"line3": ""
-	}
+	},
+	{
+		"line1": "Friday November 1st 18:30 COMP-A32:",
+		"line2": "CyberTeam Meet-Up",
+		"line3": ""
+	},
 ]
