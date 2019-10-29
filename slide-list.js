eb5800341ce282678fd2dc55cb44359c0ae7d20f
James C <3661681+JamesMCo@users.noreply.github.com>
2019-10-29 16:49:49 +0000

    Update slides for C# with Unity MLH Localhost workshop

--- slide-list.js
+++ slide-list.js
@@ -1,10 +1,9 @@
 slides = [
     "slides/general/hacksoc.html",
-    "slides/python-workshops/python.html",
-    "slides/python-workshops/python-worksheet.html",
+    "slides/localhost/csharp-with-unity.html",
+    "slides/localhost/checkin.html",
     "slides/general/social.html",
     "upcomingevents",
     "slides/general/hoodies.html",
-    "slides/general/elections.html",
     "slides/general/upcomingevents_hacknotts.html"
 ];
--- upcoming-events.js
+++ upcoming-events.js
@@ -1,15 +1,5 @@
 upcoming_events = [
 	{
-		"line1": "Monday October 28th 18:30, COMP-C60:",
-		"line2": "Extraordinary General Meeting",
-		"line3": "hacksoc.net/nominate"
-	},
-	{
-		"line1": "Tuesday October 29th 6:30pm, COMP-A32:",
-		"line2": "MLH Localhost",
-		"line3": "Learn C%23 With Unity"
-	},
-	{
 		"line1": "Thursday, October 31st 6:30pm, COMP-A32:",
 		"line2": "Hardware Meetup",
 		"line3": ""
