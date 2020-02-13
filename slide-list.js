52aa00699c87576f1aba0163a8a2a8af62e880da
James C <3661681+JamesMCo@users.noreply.github.com>
2020-02-13 18:38:55 +0000

    Update slides for "Building and Breaking"/"Alexa.Net" double speaker event

--- slide-list.js
+++ slide-list.js
@@ -1,6 +1,6 @@
 slides = [
     "slides/general/hacksoc.html",
-    "slides/badcodeathon/badcodeathon.html",
+    "slides/speaker-event/2020-02-13-doublespeakerevent.html",
     "slides/general/social.html",
     "upcomingevents",
     "slides/general/hacknotts_organiser_apps.html",
--- upcoming-events.js
+++ upcoming-events.js
@@ -1,10 +1,5 @@
 upcoming_events = [
     {
-        "line1": "Thursday, February 13th 7:00pm, COMP-A32",
-        "line2": "Speaker Night",
-        "line3": "Talks from Mark Goodwin %26 Steve Pears"
-    },
-    {
         "line1": "February 15th - February 16th",
         "line2": "CovHack",
         "line3": "Details at hacksoc.net/covhack-2020"
