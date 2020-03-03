657552ecc6e794bf75e797fc54024da23539328e
Aaron Osher <git@aaronosher.io>
2020-03-03 17:38:01 +0000

    Add sustainability week

--- slide-list.js
+++ slide-list.js
@@ -5,5 +5,6 @@ slides = [
     "slides/general/social.html",
     "upcomingevents",
     "slides/general/hacknotts_organiser_apps.html",
-    "slides/general/hoodies.html"
+    "slides/general/hoodies.html",
+    "slides/sustainability-week/sustainability-week.html"
 ];
--- upcoming-events.js
+++ upcoming-events.js
@@ -10,6 +10,11 @@ upcoming_events = [
         "line3": ""
     },
     {
+        "line1": "Monday, March 9th 6:30pm, Antenna",
+        "line2": "Tech Nottingham",
+        "line3": "Depart from Exchange Bus Stop at 17:45"
+    },
+    {
         "line1": "Tuesday, March 24th 7:00pm, EXCHGE-LT3",
         "line2": "Annual General Meeting",
         "line3": "hacksoc.net/nominate | hacksoc.net/amend"
