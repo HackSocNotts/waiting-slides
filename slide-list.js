9338fcd434c043aa99dc08d55b20aef615c5f248
James C <3661681+JamesMCo@users.noreply.github.com>
2020-03-03 16:06:56 +0000

    Update slides for Accessibility in Tech event

--- slide-list.js
+++ slide-list.js
@@ -1,6 +1,7 @@
 slides = [
     "slides/general/hacksoc.html",
-    "slides/programming-cafe/programming-cafe.html",
+    "slides/speaker-event/2020-03-03-accessibilityintech-hexproductions.html",
+    "slides/speaker-event/2020-03-03-accessibilityintech-bbccape.html",
     "slides/general/social.html",
     "upcomingevents",
     "slides/general/hacknotts_organiser_apps.html",
--- upcoming-events.js
+++ upcoming-events.js
@@ -1,6 +1,6 @@
 upcoming_events = [
     {
-        "line1": "Friday, February 28th 7:00pm, COMP-C60",
+        "line1": "Friday, March 6th 7:00pm, COMP-C60",
         "line2": "Cyber Team Meetup",
         "line3": ""
     },
@@ -8,5 +8,10 @@ upcoming_events = [
         "line1": "Saturday, March 7th 1:00pm, COMP-A32",
         "line2": "Kotlin - From Start to Somewhere",
         "line3": ""
+    },
+    {
+        "line1": "Tuesday, March 24th 7:00pm, EXCHGE-LT3",
+        "line2": "Annual General Meeting",
+        "line3": "hacksoc.net/nominate | hacksoc.net/amend"
     }
 ]
