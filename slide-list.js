a232e72f20c7ef6b2773224c47c0db9e14e2ace8
James C <3661681+JamesMCo@users.noreply.github.com>
2020-03-09 13:39:39 +0000

    Update slides for Hardware Meetup event

--- slide-list.js
+++ slide-list.js
@@ -1,9 +1,8 @@
 slides = [
     "slides/general/hacksoc.html",
-    "slides/kotlin-workshops/kotlin.html",
+    "slides/hardware/hardware.html",
     "slides/general/social.html",
     "upcomingevents",
     "slides/general/hacknotts_organiser_apps.html",
-    "slides/general/hoodies.html",
-    "slides/sustainability-week/sustainability-week.html"
+    "slides/general/hoodies.html"
 ];
--- upcoming-events.js
+++ upcoming-events.js
@@ -1,8 +1,8 @@
 upcoming_events = [
     {
-        "line1": "Monday, March 9th 6:30pm, Antenna",
-        "line2": "Tech Nottingham",
-        "line3": "Depart from Exchange Bus Stop at 17:45"
+        "line1": "Friday, March 13th 7:00pm, COMP-C60",
+        "line2": "Cyber Team Meetup",
+        "line3": ""
     },
     {
         "line1": "Tuesday, March 24th 7:00pm, EXCHGE-LT3",
