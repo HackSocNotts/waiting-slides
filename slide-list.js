b3ad255720f6a4448cf1b919bb022c296e13c099
Aaron Osher <git@aaronosher.io>
2020-06-30 17:36:45 +0100

    Update slide-list.js
    Remove hoodies

--- slide-list.js
+++ slide-list.js
@@ -4,5 +4,5 @@ slides = [
     "slides/general/social.html",
     "upcomingevents",
     "slides/general/hacknotts_organiser_apps.html",
-    "slides/general/hoodies.html"
+//     "slides/general/hoodies.html"
 ];
