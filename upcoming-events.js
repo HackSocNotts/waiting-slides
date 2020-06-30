ad1b0b733a9328ff3706c4d4ad6d0d5f8c9937ae
Peter Taylor <peter@peterhenryemerson.co.uk>
2020-06-30 16:03:17 +0100

    Removed extraneous upcoming events

--- upcoming-events.js
+++ upcoming-events.js
@@ -1,17 +1,3 @@
 upcoming_events = [
-    {
-        "line1": "Thursday, March 19th 7:00pm, COMP-A32",
-        "line2": "Programming Café",
-        "line3": ""
-    },
-    {
-        "line1": "Friday, March 13th 7:00pm, COMP-C60",
-        "line2": "Cyber Team Meetup",
-        "line3": ""
-    },
-    {
-        "line1": "Tuesday, March 24th 7:00pm, EXCHGE-LT3",
-        "line2": "Annual General Meeting",
-        "line3": "hacksoc.net/nominate | hacksoc.net/amend"
-    }
+    
 ]
