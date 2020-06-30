1205194bc849cf82d9e00ab484f80f23b1348a30
Daniel Cordell <danielbc@hotmail.co.uk>
2020-06-30 16:17:41 +0100

    Update slide-list.js

--- slide-list.js
+++ slide-list.js
@@ -1,9 +1,8 @@
 slides = [
     "slides/general/hacksoc.html",
-    "slides/hardware/hardware.html",
+    "slides/speaker-event/2020-06-30-markgoodwin-makestuff.html",
     "slides/general/social.html",
     "upcomingevents",
     "slides/general/hacknotts_organiser_apps.html",
-    "slides/general/hoodies.html",
-    "slides/speaker-event/2020-06-30-markgoodwin-makestuff.html"
+    "slides/general/hoodies.html"
 ];
