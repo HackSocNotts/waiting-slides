a1ac86625d1058a0f8874f72dbd5d3cce4d3bb37
Aaron Osher <git@aaronosher.io>
2020-09-24 18:24:23 +0100

    Update for 2020 Try-it

--- slide-list.js
+++ slide-list.js
@@ -1,8 +1,7 @@
 slides = [
-    "slides/general/hacksoc.html",
-    "slides/speaker-event/2020-06-30-markgoodwin-makestuff.html",
-    "slides/general/social.html",
-    "upcomingevents",
-    "slides/general/hacknotts_organiser_apps.html",
-//     "slides/general/hoodies.html"
+  "slides/general/hacksoc.html",
+  "slides/speaker-event/2020-09-24-andy-stanford-clark.html",
+  "slides/general/social.html",
+  "upcomingevents",
+  "slides/general/upcomingevents_hacknotts.html",
 ];
--- upcoming-events.js
+++ upcoming-events.js
@@ -1,3 +1,17 @@
 upcoming_events = [
-    
-]
+  {
+    line1: "Tuesday, September 29 7:00 PM, Online",
+    line2: "Web API workshop",
+    line3: "Presented by Peter Taylor",
+  },
+  {
+    line1: "Friday, October 9 7:00 PM, Online",
+    line2: "Cyber Security Workshop",
+    line3: "Presented by Ryan Hardwick",
+  },
+  {
+    line1: "Tuesday, October 13 7:00 PM, Online",
+    line2: ".NET Core on the Raspberry Pi",
+    line3: "Presented by Pete Gallagher",
+  },
+];
