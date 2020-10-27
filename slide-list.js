4d12841d752671f8c63ca6340ca6e000afb3dc72
Aaron Osher <git@aaronosher.io>
2020-10-27 18:04:53 +0000

    Update for 2020-10-27

--- slide-list.js
+++ slide-list.js
@@ -1,6 +1,6 @@
 slides = [
   "slides/general/hacksoc.html",
-  "slides/general-workshops/2020-10-20-discord.html",
+  "slides/general-workshops/2020-10-27-portfolio1.html",
   "slides/general/social.html",
   "upcomingevents",
   "slides/general/upcomingevents_hacknotts.html",
--- upcoming-events.js
+++ upcoming-events.js
@@ -1,15 +1,5 @@
 upcoming_events = [
   {
-    line1: "Friday, October 23 7:00 PM, Online",
-    line2: "Pen Testing the Enteral Blue Machine Workshop",
-    line3: "Presented by Ryan Hardwick",
-  },
-  {
-    line1: "Tuesday, October 27 7:00 PM, Online",
-    line2: "Build and Host Your Portfolio Website for Free",
-    line3: "Presented by Aaron Osher",
-  },
-  {
     line1: "Friday, October 30 7:00 PM, Online",
     line2: "Pen Testing on Try Hack Me",
     line3: "Presented by Ryan Hardwick",
@@ -18,5 +8,15 @@ upcoming_events = [
     line1: "Tuesday, November 3 7:00 PM, Online",
     line2: "Building for Slack in .NET",
     line3: "Presented by Steven Pears",
+  },
+  {
+    line1: "Tuesday, November 26 7:00 PM, Online",
+    line2: "The (connected) house of horrors",
+    line3: "Presented by Terence Eden",
+  },
+  {
+    line1: "Tuesday, December 08 7:00 PM, Online",
+    line2: "Automotive cyber security",
+    line3: "Presented by Aileen Ryan",
   },
 ];
