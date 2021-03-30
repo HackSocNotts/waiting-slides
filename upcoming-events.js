cc3dce2815b43fbc963e9e6c039e876d83154edb
Peter Taylor <peter@peterhenryemerson.co.uk>
2021-03-30 10:09:44 +0100

    Update upcoming-events.js

--- upcoming-events.js
+++ upcoming-events.js
@@ -1,22 +1,7 @@
 upcoming_events = [
   {
-    line1: "Friday, March 12 7:00 PM, Online",
+    line1: "Friday, 2nd April 7:00 PM, Online",
     line2: "Pen Testing on Try Hack Me",
     line3: "Presented by Ryan Hardwick",
-  },
-  {
-    line1: "Tuesday, March 16 7:00 PM, Online",
-    line2: "3D printed Bionic Hand a little IOT and a Xamarin Mobile App",
-    line3: "Presented by Clifford Agius",
-  },
-  {
-    line1: "Friday, March 19 7:00 PM, Online",
-    line2: "Pen Testing on Try Hack Me",
-    line3: "Presented by Ryan Hardwick",
-  },
-  {
-    line1: "Tuesday, March 30 7:00 PM, Online",
-    line2: "Computerphile, Hello World!",
-    line3: "Presented by Sean Riley",
-  },
+  }
 ];
