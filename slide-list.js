6b15d86eebecc1113d7cef370a47145a4c690a9f
Peter Taylor <peter@peterhenryemerson.co.uk>
2021-03-30 10:08:36 +0100

    Update slide-list.js

--- slide-list.js
+++ slide-list.js
@@ -1,6 +1,6 @@
 slides = [
   "slides/general/hacksoc.html",
-  "slides/general-workshops/2021-03-09-discord-bots.html",
+  "slides/general-workshops/2021-03-30-hello-computerphile.html",
   "slides/general/social.html",
   "upcomingevents",
   "slides/general/membershiprequired.html",
